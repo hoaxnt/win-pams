fe92649f82ff8df78391d6f8f82ed1514ea49936
dev-laww <lawrenceallen.tech@gmail.com>
2024-06-11 15:45:05 +0800

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "com.winpams"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.cdimascio:dotenv-java:2.2.0")
+    implementation("mysql:mysql-connector-java:8.4.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
