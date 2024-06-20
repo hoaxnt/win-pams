ad88566bb3777d14c5f5cb12ef3e17106e315ad1
dev-laww <lawrenceallen.tech@gmail.com>
2024-06-20 23:40:24 +0800

    feat: added name on main panel

--- src/main/java/com/winpams/user/ui/Feed.java
+++ src/main/java/com/winpams/user/ui/Feed.java
@@ -6,6 +6,7 @@ public class Feed {
 
     private JLabel MENU;
     private JButton button1;
+    JPanel panel;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
