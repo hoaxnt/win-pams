7a45eda299a386dd7e55d39fb9bc2177594cd1a6
kyle <ambilllkyleee@gmail.com>
2024-06-21 16:01:22 +0800

    create approval and overview form

---
+++ src/main/java/com/winpams/admin/ApprovalPanel.java
@@ -0,0 +1,11 @@
+package com.winpams.admin;
+
+import javax.swing.*;
+
+public class ApprovalPanel {
+    private JButton approveButton;
+    private JButton rejectButton;
+    private JButton cancelButton;
+    private JTable RequestTable;
+    private JButton viewApplicationButton;
+}
---
+++ src/main/java/com/winpams/admin/Overview.java
@@ -0,0 +1,12 @@
+package com.winpams.admin;
+
+import javax.swing.*;
+
+public class Overview {
+    private JButton overviewButton;
+    private JButton managePetButton;
+    private JButton requestApplicationButton;
+    private JButton userManagementButton;
+    private JButton logOutButton;
+    private JScrollPane scrollPane1;
+}
